a679ca938ede6f7d48ae6d5b60cd34dadabc8205
Tendi <pastendisherpa26@gmail.com>
2023-01-29 15:32:28 +0545

    Router setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Navbar, Sidebar, Footer } from './components/layouts'
+import {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Checkout,
+  Error,
+  PrivateRoute,
+} from './pages'
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello world</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route path='/'>
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='products' element={<Products />} />
+          <Route path='products/:id' element={<SingleProduct />} />
+          <Route path='cart' element={<Cart />} />
+          <Route
+            path='checkout'
+            element={
+              <PrivateRoute>
+                <Checkout />
+              </PrivateRoute>
+            }
+          />
+        </Route>
+        <Route path='*' element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
   )
 }
 
---
+++ src/components/layouts/index.js
@@ -0,0 +1,4 @@
+import Navbar from './Navbar'
+import Sidebar from './Sidebar'
+import Footer from './Footer'
+export { Navbar, Sidebar, Footer }
