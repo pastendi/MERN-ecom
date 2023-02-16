8b4d06ac3b720de7b07797420e9af9660feb6f32
Tendi <pastendisherpa26@gmail.com>
2023-02-16 22:13:43 +0545

    Auth0 and userContext setup

---
+++ src/context/UserContext.js
@@ -0,0 +1,21 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
+
+const UserContext = React.createContext()
+
+export const UserProvider = ({ children }) => {
+  const { isAuthenticated, loginWithRedirect, user, logout, isLoading } =
+    useAuth0()
+  const [myUser, setMyUser] = useState(null)
+  useEffect(() => {
+    console.log({ user, isAuthenticated, isLoading })
+  }, [isAuthenticated])
+  return (
+    <UserContext.Provider value={{ loginWithRedirect, logout, myUser }}>
+      {children}
+    </UserContext.Provider>
+  )
+}
+export const useUserContext = () => {
+  return useContext(UserContext)
+}
--- src/index.js
+++ src/index.js
@@ -6,16 +6,26 @@ import App from './App'
 import { ProductsProvider } from './context/ProductsContext'
 import { FilterProvider } from './context/FilterContext'
 import { CartProvider } from './context/CartContext'
-
+import { Auth0Provider } from '@auth0/auth0-react'
+import { UserProvider } from './context/UserContext'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <ProductsProvider>
-      <FilterProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </FilterProvider>
-    </ProductsProvider>
+    <Auth0Provider
+      domain='dev-07d4zmy0tqt4towe.us.auth0.com'
+      clientId='uGRysyUIknovCfpNbRCEi48c7QHw5Zmu'
+      redirectUri={window.location.origin}
+      cacheLocation='localstorage'
+    >
+      <UserProvider>
+        <ProductsProvider>
+          <FilterProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </FilterProvider>
+        </ProductsProvider>
+      </UserProvider>
+    </Auth0Provider>
   </React.StrictMode>
 )
